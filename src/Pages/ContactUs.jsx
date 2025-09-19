ca6ae00672252ab90d7f2e337e8d57f5acf74454
Ebu-costain <ebukaiwuchukwuconstantine@gmail.com>
2025-09-19 11:55:18 +0100

    my first commit

--- src/Pages/ContactUs.jsx
+++ src/Pages/ContactUs.jsx
@@ -27,8 +27,7 @@ const ContactUs = () => {
       <div className="relative max-w-6xl mx-auto px-6 py-20 font-sans z-10">
         {/* Section Header */}
         <div className="text-center mb-12">
-          <motion.h1
-            className="text-4xl font-bold text-gray-900"
+          <motion.h1 className="text-4xl font-bold text-gray-900"
             initial={{ opacity: 0, y: -20 }}
             whileInView={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.6 }}
